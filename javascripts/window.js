677a4a1735a168a994734ab6aee50e38c466f354
ajaximrpg <ajaximrpg@svexpertise.com>
2013-07-15 10:30:38 -0700

    use jQuery filter() method

--- javascripts/window.js
+++ javascripts/window.js
@@ -3,7 +3,7 @@
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
+// without limitation the rights to use, copy, fmodify, merge, publish,
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
@@ -1343,7 +1343,7 @@ var Windows = {
   
   // Unregisters a window (called by Windows destructor)
   unregister: function(win) {
-    this.windows = this.windows.filter( function() { return this!=win; });
+    this.windows = $(this.windows).filter( function() { return this!=win; });
   }, 
   
   // Closes all windows
